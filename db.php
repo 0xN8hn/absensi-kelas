7ddd280345aec1089283baa2edc091478af31e15
0xN8hn <nathisalwaysright@gmail.com>
2026-04-06 11:29:11 +0700

    f

--- db.php
+++ db.php
@@ -6,5 +6,15 @@ $name = getenv('MYSQLDATABASE') ?: 'railway';
 $port = getenv('MYSQLPORT')     ?: 3306;
 
 $conn = new mysqli($host, $user, $pass, $name, (int)$port);
+
+if ($conn->connect_error) {
+    header('Content-Type: application/json');
+    die(json_encode([
+        'ok'  => false,
+        'msg' => 'Koneksi DB gagal: ' . $conn->connect_error
+    ]));
+}
+
 $conn->set_charset('utf8mb4');
+
 ?>
